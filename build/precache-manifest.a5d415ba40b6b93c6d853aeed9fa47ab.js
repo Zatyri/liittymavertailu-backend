a5da9b9a48eb846deda6624ab651188680c04999
Oskar <gustosk@gmail.com>
2020-09-08 11:54:15 +0300

    Added frontend build

---
+++ build/precache-manifest.a5d415ba40b6b93c6d853aeed9fa47ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dffef986252bc76848f4d4bf62a05b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfee2a874eb11c5c3e1b",
+    "url": "/static/css/main.820bdd21.chunk.css"
+  },
+  {
+    "revision": "39d5b0fd0c9538fac199",
+    "url": "/static/js/2.8a50048b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8a50048b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfee2a874eb11c5c3e1b",
+    "url": "/static/js/main.1d289c16.chunk.js"
+  },
+  {
+    "revision": "41a9348c6c498c346805",
+    "url": "/static/js/runtime-main.4eb76e2c.js"
+  },
+  {
+    "revision": "fc91396f1252f8953906508350fe1978",
+    "url": "/static/media/Elisa_logo.fc91396f.png"
+  },
+  {
+    "revision": "e8d056bb2d3bd50208a16afba35e277f",
+    "url": "/static/media/Telia_logo.e8d056bb.png"
+  }
+]);
